0e4477476df2df336dbec7b8a838a2de66b37559
bondesvick <bondesvick@gmail.com>
2021-02-05 13:22:34 +0100

    First Setup commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+const BASE_URL = 'https://localhost:5001/api/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'dating-app';
+
+  users: any;
+
+
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router, private http: HttpClient) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+  this.mobileQuery.addListener(this._mobileQueryListener);
+   }
+
+   links =[
+     {path: "/home", icon: "home", title: "Home"},
+     {path: "/items", icon: "view_list", title: "Courses"},
+     {path: "/bookitems", icon: "book", title: "Books"},
+     {path: "/users", icon: "person", title: "Users"}
+    ]
+
+    logOut(){
+      this.router.navigateByUrl("/login");
+    }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get(BASE_URL).subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+  ngOnDestroy(): void {}
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,50 @@
+import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRippleModule} from '@angular/material/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+
+
+@NgModule({
+  exports: [
+   
+    MatCardModule,
+    MatInputModule,
+    MatGridListModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRippleModule,
+    MatSnackBarModule,
+    MatBottomSheetModule
+  ]
+})
+export class MaterialModule {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
